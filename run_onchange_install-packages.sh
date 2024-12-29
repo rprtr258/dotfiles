6eb6ed031d8e329da25b86bb271842455aa41ef5
Poletansky Viktor <poletansky@gmail.com>
2024-12-29 09:03:54 +0300

    Update install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -5,10 +5,19 @@ sudo apt install git ripgrep flatpak curl wget polybar fonts-liberation rofi ima
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.telegram.desktop
 
-wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
-rm go1.23.4.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
+check_go_installed() {
+  if [ -z "$(command -v go)" ] || [ "$(go version)" != "go version go1.23.4 linux/amd64" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+if check_go_installed; then
+  wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
+  sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
+  rm go1.23.4.linux-amd64.tar.gz
+  go version
+else
+fi
 go install github.com/junegunn/fzf@latest
 
