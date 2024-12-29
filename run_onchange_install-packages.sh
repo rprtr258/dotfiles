077ef706687fc02bef24574dd9718f527eb27038
Poletansky Viktor <poletansky@gmail.com>
2024-12-29 08:39:12 +0300

    Update install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo apt install git ripgrep
+sudo apt install git ripgrep flatpak curl alacritty polybar fonts-liberation rofi imagemagick feh parcellite xclip
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.telegram.desktop
 
